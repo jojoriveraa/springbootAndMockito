17c6c407d8048d8aa425b5e96f2389914ba0c967
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-10-16 12:52:37 -0500

    Add SomeDataService call

--- src/main/java/com/walmart/det/unittesting/unittesting/business/SomeBusinessImpl.java
+++ src/main/java/com/walmart/det/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -2,10 +2,23 @@ package com.walmart.det.unittesting.unittesting.business;
 
 import java.util.Arrays;
 
+import com.walmart.det.unittesting.unittesting.data.SomeDataService;
+
 public class SomeBusinessImpl {
-	
-	public int calculateSum(int [] data) {
+
+	private SomeDataService someDataService;
+
+	public void setSomeDataService(SomeDataService someDataService) {
+		this.someDataService = someDataService;
+	}
+
+	public int calculateSum(int[] data) {
 		return Arrays.stream(data).sum();
+	}
+
+	public int calculateSumFromDataService() {
+		int data[] = someDataService.retriveAllData();
+		return calculateSum(data);
 	}
 
 }
