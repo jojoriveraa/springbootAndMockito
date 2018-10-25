f3697bd7a7dbbfd9c49438a7d3a9c06b8c43c183
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-10-25 09:44:29 -0500

    Update src/main/java/com/walmart/det/unittesting/unittesting/controller/HelloWorldController.java

--- src/main/java/com/walmart/det/unittesting/unittesting/controller/HelloWorldController.java
+++ src/main/java/com/walmart/det/unittesting/unittesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.walmart.det.unittesting.unittesting;
+package com.walmart.det.unittesting.unittesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
