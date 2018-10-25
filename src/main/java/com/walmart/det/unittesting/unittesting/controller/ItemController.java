ef35556bfa65a3e3c3b177459a17665578289763
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-10-25 10:27:03 -0500

    Create Item Controller

---
+++ src/main/java/com/walmart/det/unittesting/unittesting/controller/ItemController.java
@@ -0,0 +1,16 @@
+package com.walmart.det.unittesting.unittesting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.walmart.det.unittesting.unittesting.model.Item;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/dummy-item")
+    public Item dummyItem() {
+        return new Item(1, "Ball", 1, 100);
+    }
+
+}
