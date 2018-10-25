dd19d2395702733793fed056b1450ddba04da053
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-10-25 10:26:34 -0500

    Update src/main/java/com/walmart/det/unittesting/unittesting/controller/HelloWorldController.java
    Fix camelCase

--- src/main/java/com/walmart/det/unittesting/unittesting/controller/HelloWorldController.java
+++ src/main/java/com/walmart/det/unittesting/unittesting/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
-    public String HelloWorld() {
+    public String helloWorld() {
         return "Hello world";
     }
 
