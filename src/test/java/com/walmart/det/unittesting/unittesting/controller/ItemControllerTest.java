45741243732b8abc988f6eb7f87409e32ebb4145
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-10-26 22:46:23 -0500

    Update src/test/java/com/walmart/det/unittesting/unittesting/controller/ItemControllerTest.java

--- src/test/java/com/walmart/det/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/walmart/det/unittesting/unittesting/controller/ItemControllerTest.java
@@ -24,7 +24,7 @@ public class ItemControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@MockBean
 	private ItemBusinessService businessService;
 
@@ -33,39 +33,38 @@ public class ItemControllerTest {
 		RequestBuilder request = MockMvcRequestBuilders
 				.get("/dummy-item")
 				.accept(MediaType.APPLICATION_JSON);
-		
+
 		mockMvc.perform(request)
 				.andExpect(status().isOk())
 				.andExpect(content().json("{\"id\": 1,\"name\":\"Ball\",\"price\":1,\"quantity\":100}"))
 				.andReturn();
 	}
-	
+
 	@Test
 	public void dummyItem_noCheckPrice() throws Exception {
 		RequestBuilder request = MockMvcRequestBuilders
 				.get("/dummy-item")
 				.accept(MediaType.APPLICATION_JSON);
-		
+
 		mockMvc.perform(request)
 				.andExpect(status().isOk())
 				.andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"quantity\":100}"))
 				.andReturn();
 	}
-	
+
 	@Test
 	public void itemFromBusinessService_basic() throws Exception {
-		when(businessService.retriveHardCodedItem()).thenReturn(
-				new Item(2, "Item2", 10, 10));
-		
+		when(businessService.retriveHardCodedItem()).thenReturn(new Item(2, "Item2", 10, 10));
+
 		RequestBuilder request = MockMvcRequestBuilders
 				.get("/item-from-business-service")
 				.accept(MediaType.APPLICATION_JSON);
-		
+
 		mockMvc.perform(request)
 				.andExpect(status().isOk())
 				.andExpect(content().json("{id:2,name:Item2,price:10,quantity:10}"))
 				.andReturn();
-		
+
 	}
 
 }
