7454c206b7fa87ce0cd8d252cf15ba4ac5063e34
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-10-29 16:44:50 -0600

    Create test to retrive all items feature

--- src/test/java/com/walmart/det/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/walmart/det/unittesting/unittesting/controller/ItemControllerTest.java
@@ -4,6 +4,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,7 +56,10 @@ public class ItemControllerTest {
 
 	@Test
 	public void itemFromBusinessService_basic() throws Exception {
-		when(businessService.retriveHardCodedItem()).thenReturn(new Item(2, "Item2", 10, 10));
+		when(businessService.retriveHardCodedItem())
+				.thenReturn(
+						new Item(2, "Item2", 10, 10)
+						);
 
 		RequestBuilder request = MockMvcRequestBuilders
 				.get("/item-from-business-service")
@@ -63,6 +68,25 @@ public class ItemControllerTest {
 		mockMvc.perform(request)
 				.andExpect(status().isOk())
 				.andExpect(content().json("{id:2,name:Item2,price:10,quantity:10}"))
+				.andReturn();
+
+	}
+	
+	@Test
+	public void retriveAllItems_basic() throws Exception {
+		when(businessService.retrieveAllItems()).thenReturn(
+				Arrays.asList(
+						new Item(2, "Item2", 10, 10),
+						new Item(3, "Item3", 20, 20)
+						));
+
+		RequestBuilder request = MockMvcRequestBuilders
+				.get("/all-items-from-database")
+				.accept(MediaType.APPLICATION_JSON);
+
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().json("[{id:2,name:Item2,price:10,quantity:10},{id:3,name:Item3}]"))
 				.andReturn();
 
 	}
